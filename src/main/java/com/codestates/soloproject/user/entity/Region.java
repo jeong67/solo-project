4856d2da5182f76fd4af95a61d9ec35566b4572f
Min <dobee1016@gmail.com>
2022-08-19 00:03:32 +0900

    Entity 수정

--- src/main/java/com/codestates/soloproject/user/entity/Region.java
+++ src/main/java/com/codestates/soloproject/user/entity/Region.java
@@ -1,4 +1,22 @@
 package com.codestates.soloproject.user.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "REGION_TABLE")
 public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long regionId;
+
+    @Column
+    private String region;
 }
--- src/main/java/com/codestates/soloproject/user/entity/Sector.java
+++ src/main/java/com/codestates/soloproject/user/entity/Sector.java
@@ -1,4 +1,25 @@
 package com.codestates.soloproject.user.entity;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "SECTOR_TABLE")
 public class Sector {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long sectorId;
+
+    @Column
+    private String sector;
+
+
 }
